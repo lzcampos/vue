bfe5218b5e9ff2c100187bccf2ba91e742e8660d
Luiz Damasceno <luizedc1@gmail.com>
2021-01-20 23:43:38 -0300

    Feat: adding comments in clickOutside directive

--- directives/clickOutside.js
+++ directives/clickOutside.js
@@ -1,3 +1,6 @@
+/* Directive made for helping you to know if a click was made inside a
+element or not. To use it, call it passing your click event and a className string.
+*/
 const ClickOutside = (event, className) => {
     let clickedInside = false
     Object.entries(event.path).forEach(entry => {
